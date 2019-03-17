feb88ce4a8edeebb208ba684e2a21a6417fef51e
Aravind R Pillai <aravindrpillai1992@gmail.com>
2019-03-17 19:28:37 +0530

    core codeignitor code

---
+++ application/config/autoload.php
@@ -0,0 +1,9 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+$autoload['packages'] = array();
+$autoload['libraries'] = array();
+$autoload['drivers'] = array();
+$autoload['config'] = array();
+$autoload['language'] = array();
+$autoload['model'] = array();
+$autoload['helper'] = array('url');

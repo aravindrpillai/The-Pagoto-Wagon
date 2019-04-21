a962ef7eee956bd3dfca5c364fa9358ccc179b80
Aravind R Pillai <aravindrpillai1992@gmail.com>
2019-04-21 21:49:12 +0530

    login

---
+++ application/views/login.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Pagoto Login</title>
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <link rel="stylesheet" href="<?php echo base_url('assets/bootstrap/bower_components/bootstrap/dist/css/bootstrap.min.css'); ?>" >
+  <link rel="stylesheet" href="<?php echo base_url('assets/bootstrap/bower_components/font-awesome/css/font-awesome.min.css'); ?>" >
+  <link rel="stylesheet" href="<?php echo base_url('assets/bootstrap/bower_components/Ionicos/css/ionicons.min.css'); ?>" >
+  <link rel="stylesheet" href="<?php echo base_url('assets/bootstrap/dist/css/AdminLTE.min.css'); ?>" >
+  <link rel="stylesheet" href="<?php echo base_url('assets/bootstrap/plugins/iCheck/square/blue.css'); ?>" >
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+</head>
+
+<body class="login-page"  style="height:150px!important">
+<div class="login-box">
+  <div class="login-logo">
+    <a>The<b>Pagoto</b>Wagon</a>
+  </div>
+  <div class="login-box-body">
+    <p class="login-box-msg">Sign in to start your session</p>
+
+    <form action="<?php echo base_url("login/authenticate") ?>" method="post">
+      <div class="form-group has-feedback">
+        <input type="email" class="form-control" placeholder="Username">
+        <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+      </div>
+      <div class="form-group has-feedback">
+        <input type="password" class="form-control" placeholder="Password">
+        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+      </div>
+      <div class="row">
+        <div class="col-xs-8">
+          <div class="checkbox icheck">
+            <label>
+              <input type="checkbox"> Remember Me
+            </label>
+          </div>
+        </div>
+        <div class="col-xs-4">
+          <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
+        </div>
+      </div>
+    </form>
+  </div>
+  <hr>
+  <a>Version : 1.0 | Credits: Aravind (9447020535)</a>
+</div>
+
+<script src="<?php echo base_url('assets/bootstrap/bower_components/jquery/dist/jquery.min.js') ?>"></script>
+<script src="<?php echo base_url('assets/bootstrap/dist/js/bootstrap.min.js') ?>"></script>
+<script src="<?php echo base_url('assets/bootstrap/plugins/iCheck/icheck.min.js') ?>"></script>
+<script>
+  $(function () {
+    $('input').iCheck({
+      checkboxClass: 'icheckbox_square-blue',
+      radioClass: 'iradio_square-blue',
+      increaseArea: '20%' /* optional */
+    });
+  });
+</script>
+</body>
+</html>

fcee8d397258d1407353e522f2007bada34f22d2
Aravind R Pillai <aravindrpillai1992@gmail.com>
2019-04-23 00:33:46 +0530

    database

--- related_files/pagotodb.sql
+++ related_files/pagotodb.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 22, 2019 at 05:48 PM
+-- Generation Time: Apr 22, 2019 at 09:03 PM
 -- Server version: 10.1.38-MariaDB
--- PHP Version: 7.3.4
+-- PHP Version: 5.6.40
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -21,6 +21,25 @@ SET time_zone = "+00:00";
 --
 -- Database: `pagotodb`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sequence_util`
+--
+
+CREATE TABLE `sequence_util` (
+  `id` int(11) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `value` bigint(20) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `sequence_util`
+--
+
+INSERT INTO `sequence_util` (`id`, `name`, `value`) VALUES
+(1, 'employee_id', 11008);
 
 -- --------------------------------------------------------
 
@@ -43,11 +62,12 @@ CREATE TABLE `shops` (
 INSERT INTO `shops` (`id`, `name`, `place`, `is_open`, `start_date`) VALUES
 (3, 'Pagoto Wagon', 'Kawadiyaar', 1, '2019-02-01'),
 (4, 'Pagoto Wagon', 'Kothamangalam', 1, '2019-04-02'),
-(5, 'Pagoto Wagon 2', 'Kochi', 0, '2019-04-11'),
+(5, 'Pagoto Wagon 2', 'Kochi', 1, '2019-04-11'),
 (7, 'Pagoto Wagon 2', 'kollam', 1, '2019-04-11'),
 (9, 'Pagoto Wagon', 'Angamaly', 1, '2019-04-03'),
 (17, 'Pagoto Wagon', 'Kottayam', 0, '2019-04-06'),
-(23, 'Pagoto Wagon', 'Wayanad', 0, '2019-04-08');
+(23, 'Pagoto Wagon', 'Wayanad', 0, '2019-04-08'),
+(24, 'Pagoto Wagon', 'Yeroor', 1, '2019-04-04');
 
 -- --------------------------------------------------------
 
@@ -61,7 +81,6 @@ CREATE TABLE `users` (
   `username` varchar(25) NOT NULL,
   `password` varchar(25) NOT NULL,
   `is_active` tinyint(1) NOT NULL DEFAULT '0',
-  `dp` varchar(100) DEFAULT NULL,
   `enable_always_logged_in` tinyint(1) NOT NULL DEFAULT '0',
   `last_logged_in` datetime DEFAULT NULL,
   `employee_id` varchar(20) DEFAULT NULL,
@@ -71,21 +90,32 @@ CREATE TABLE `users` (
   `aadhar_number` varchar(20) DEFAULT NULL,
   `employement_start_date` date DEFAULT NULL,
   `retired` tinyint(1) NOT NULL DEFAULT '0',
-  `emplyement_end_date` date DEFAULT NULL
+  `emplyement_end_date` date DEFAULT NULL,
+  `mobile_number` varchar(10) DEFAULT NULL,
+  `dp` varchar(100) NOT NULL DEFAULT 'user.jpg'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `username`, `password`, `is_active`, `dp`, `enable_always_logged_in`, `last_logged_in`, `employee_id`, `is_master`, `is_admin`, `is_biller`, `aadhar_number`, `employement_start_date`, `retired`, `emplyement_end_date`) VALUES
-(1, 'Pagoto Master User', 'pagoto', 'pagoto', 0, 'admin.png', 0, '2019-04-21 05:23:29', 'MASTER', 1, 0, 0, 'MASTER', '2019-04-01', 0, NULL),
-(2, 'Aravind R Pillai', 'aravind', 'aravind', 0, '2345678876533456787654.jpg', 0, '2019-04-21 00:00:00', 'P10002', 0, 1, 1, '3345-6654-3456-3443', '2019-04-01', 0, NULL),
-(3, 'Rakesh', 'rakesh', 'rakesh', 0, NULL, 0, '2019-04-03 00:00:00', 'P100003', 0, 0, 1, '4545-8767-9878-0098', '2019-04-02', 0, NULL);
+INSERT INTO `users` (`id`, `name`, `username`, `password`, `is_active`, `enable_always_logged_in`, `last_logged_in`, `employee_id`, `is_master`, `is_admin`, `is_biller`, `aadhar_number`, `employement_start_date`, `retired`, `emplyement_end_date`, `mobile_number`, `dp`) VALUES
+(1, 'Pagoto Master User', 'pagoto', 'pagoto', 0, 0, '2019-04-21 05:23:29', 'MASTER', 1, 0, 0, 'MASTER', '2019-04-01', 0, NULL, '0000000000', 'admin.jpg'),
+(2, 'Aravind R Pillai', 'aravind', 'aravind', 0, 0, '2019-04-21 00:00:00', 'P10002', 0, 1, 1, '3345-6654-3456-3443', '2019-04-01', 0, NULL, '9447020535', '2345678876533456787654.jpg'),
+(7, 'Yusuf Ali', '11003', '6ity8a', 0, 0, NULL, 'P11003', 0, 0, 1, '2343-4543-2322-1234', '2019-04-02', 0, NULL, '9990009990', 'user.jpg'),
+(9, 'Karthik Prakash', '11005', 'tzt87g', 0, 0, NULL, 'P11005', 0, 0, 1, '3333-4444-5555-6666', '2019-04-03', 0, NULL, '8787878787', 'user.jpg'),
+(10, 'Yeshu Yahoda', '11008', 's9va2s', 0, 0, NULL, 'P11008', 0, 0, 1, '3345-6654-3456-1001', '2019-04-02', 0, NULL, '9998889997', 'user.jpg');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `sequence_util`
+--
+ALTER TABLE `sequence_util`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`);
 
 --
 -- Indexes for table `shops`
@@ -100,25 +130,31 @@ ALTER TABLE `shops`
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `username` (`username`),
-  ADD UNIQUE KEY `dp` (`dp`),
   ADD UNIQUE KEY `employee_id` (`employee_id`),
-  ADD UNIQUE KEY `aadhar_number` (`aadhar_number`);
+  ADD UNIQUE KEY `aadhar_number` (`aadhar_number`),
+  ADD UNIQUE KEY `mobile_number` (`mobile_number`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `sequence_util`
+--
+ALTER TABLE `sequence_util`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `shops`
 --
 ALTER TABLE `shops`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
